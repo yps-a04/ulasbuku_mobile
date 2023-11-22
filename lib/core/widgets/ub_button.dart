0ab45bcd8190b654c1f83c462b6980dc550a6b88
Daffa Zuhdii <daffaakmal2101@gmail.com>
2023-11-22 16:18:52 +0800

    home front end

--- lib/core/widgets/ub_button.dart
+++ lib/core/widgets/ub_button.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 
 class UBButton extends StatelessWidget {
-  const UBButton({super.key});
+  const UBButton({
+    super.key,
+    required this.height,
+    required this.width,
+    required this.text,
+    required this.primaryColor,
+    required this.secondaryColor,
+    this.icon,
+  });
+
+  final double width;
+  final double height;
+  final String text;
+  final IconData? icon;
+  final Color primaryColor;
+  final Color secondaryColor;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: height,
+      width: width,
+      decoration: BoxDecoration(
+        color: primaryColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              text,
+              style:
+                  TextStyle(color: secondaryColor, fontWeight: FontWeight.bold),
+            ),
+            if (icon != null) ...[
+              Icon(
+                icon,
+                color: secondaryColor,
+              )
+            ]
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/home/presentation/widgets/book_card.dart
+++ lib/features/home/presentation/widgets/book_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ulas_buku_mobile/core/widgets/ub_button.dart';
 
 class BookCard extends StatelessWidget {
   const BookCard(
@@ -180,52 +181,4 @@ class BookCard extends StatelessWidget {
   }
 }
 
-class UBButton extends StatelessWidget {
-  const UBButton({
-    super.key,
-    required this.height,
-    required this.width,
-    required this.text,
-    required this.primaryColor,
-    required this.secondaryColor,
-    this.icon,
-  });
 
-  final double width;
-  final double height;
-  final String text;
-  final IconData? icon;
-  final Color primaryColor;
-  final Color secondaryColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: height,
-      width: width,
-      decoration: BoxDecoration(
-        color: primaryColor,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              text,
-              style:
-                  TextStyle(color: secondaryColor, fontWeight: FontWeight.bold),
-            ),
-            if (icon != null) ...[
-              Icon(
-                icon,
-                color: secondaryColor,
-              )
-            ]
-          ],
-        ),
-      ),
-    );
-  }
-}
