f1da799fcbfd9841cd1b74c8f46071b0da878bbd
Ghaisan007 <gl.aqila04@gmail.com>
2023-12-07 15:11:46 +0700

    Berhasil implementasi delete user (finished)

--- lib/features/admin/presentation/users/list_user.dart
+++ lib/features/admin/presentation/users/list_user.dart
@@ -25,11 +25,88 @@ class _ListUserPageState extends State<ListUserPage> {
         User user = User.fromJson(i);
         list_user.add(user);
       }
+      print(response);
 
       return list_user;
     } catch (e) {
       throw Exception('error : $e');
     }
+  }
+
+  void _deleteUser(int id, CookieRequest cookieRequest) async {
+    try {
+      final response = await cookieRequest 
+          .post('http://127.0.0.1:8000/show-admin/delete-user/$id/', {});
+      if (response["status"] == true) {
+        setState(() {
+          
+        });
+      }
+      print(response);
+    } catch (e) {
+      throw Exception('error : $e');
+    }
+  }
+
+  void _showDeleteConfirmationDialog(String username, int id, CookieRequest cookieRequest) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Delete User'),
+          content: Text('Are you sure you want to delete the user $username?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                _deleteUser(id, cookieRequest);
+                Navigator.of(context).pop(); // Close the dialog
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(
+                    SnackBar(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                      behavior: SnackBarBehavior.floating,
+                      backgroundColor: Colors.white,
+                      margin: EdgeInsets.fromLTRB(width * 0.1,
+                          height * 0.1, width * 0.1, height * 0.75),
+                      content: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          const Icon(
+                            Icons.check,
+                            color: Colors.green,
+                          ),
+                          const SizedBox(
+                            width: 16,
+                          ),
+                          Text(
+                            "User $username telah dihapus !",
+                            style:
+                                const TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+              },
+              child: Text('Delete'),
+            ),
+          ],
+        );
+        
+      },
+    );
   }
 
   @override
@@ -61,7 +138,7 @@ class _ListUserPageState extends State<ListUserPage> {
               return const Column(
                 children: [
                 Text(
-                    "Tidak ada data produk.",
+                    "Tidak ada user yang telah register.",
                     style:
                         TextStyle(color: Color(0xFFffcc80), fontSize: 20),
                 ),
@@ -71,58 +148,58 @@ class _ListUserPageState extends State<ListUserPage> {
             } else {
               return ListView.builder(
                 itemCount: snapshot.data!.length,
-                itemBuilder: (_, index) => GestureDetector(
-                  onTap: () {
-                    // Routing soon
-                  },
-                  // child: Dismissible(
-                  //   key: Key('key'),
-                  //   onDismissed:(direction) {
-                      
-                  //   },
-                    child: Card(
-                      elevation: 3,
-                      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "${snapshot.data![index].username}",
-                              style: const TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
+                itemBuilder: (_, index) => Card(
+                  elevation: 3,
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // Left column with username, date joined, and last login
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "${snapshot.data![index].username}",
+                                style: const TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                            Text(
-                              "Date Joined: ${snapshot.data![index].dateJoined}",
-                              style: const TextStyle(
-                                fontSize: 18.0,
+                              const SizedBox(height: 8),
+                              Text(
+                                "Date Joined: ${snapshot.data![index].dateJoined}",
+                                style: const TextStyle(
+                                  fontSize: 18.0,
+                                ),
                               ),
-                            ),
-                            Text(
-                              "Last Login: ${snapshot.data![index].lastLogin}",
-                              style: const TextStyle(
-                                fontSize: 18.0,
+                              const SizedBox(height: 8),
+                              Text(
+                                "Last Login: ${snapshot.data![index].lastLogin}",
+                                style: const TextStyle(
+                                  fontSize: 18.0,
+                                ),
                               ),
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.delete),
-                              onPressed: () {
-                                // Add your logic for user deletion
-                                // You may want to show a confirmation dialog before deleting
-                                // and then update the UI accordingly
-                              },
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
+                        // Right column with delete button
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            // Add your logic for user deletion
+                            // You may want to show a confirmation dialog before deleting
+                            // and then update the UI accordingly
+                            _showDeleteConfirmationDialog(snapshot.data![index].username, snapshot.data![index].id, request);
+                          },
+                        ),
+                      ],
                     ),
-                  // ),
+                  ),
                 ),
               );
-              
             }
           }
         }
