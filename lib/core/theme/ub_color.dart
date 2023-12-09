5c04e00c7b605b0fcf4ecc401fb3365424af74b1
Daffa Zuhdii <daffaakmal2101@gmail.com>
2023-12-10 01:50:15 +0800

    feat(home): integ sorted books and add dark mode

---
+++ lib/core/theme/ub_color.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class UBColor {
+  static List<Color> lightCardColors = [
+    const Color(0xffacdcf2),
+    const Color.fromRGBO(249, 187, 208, 1),
+    const Color(0xffb2dfdc),
+    const Color(0xFFffcc80),
+    const Color(0xffc5cae8),
+  ];
+
+  static List<Color> darkCardColors = [
+    const Color(0xffffc68a),
+    const Color(0xff83dec4),
+    const Color(0xffa2d2fb),
+    const Color(0xffd1bcff),
+    const Color(0xff83dec4),
+  ];
+
+  static Color lightBgColor = const Color(0xfffbfaff);
+  static Color darkBgColor = const Color(0xff1b1926);
+}
