c975413a260c3ae0b702b86843825ee6fde4492b
Muhammad Iqbal <miqbalsparta2004@gmail.com>
2023-12-11 13:13:41 +0700

    bookmark

--- lib/core/environments/endpoints.dart
+++ lib/core/environments/endpoints.dart
@@ -1,6 +1,6 @@
 class EndPoints {
   //kalo pake emulator gini kalo engga pake link local yang django
-  static const String baseUrl = 'http://127.0.0.1:8000/';
+  static const String baseUrl = 'http://10.0.2.2:8000';
   static const String login = '$baseUrl/auth/login/';
   static const String logout = '$baseUrl/auth/logout/';
   static const String getAllBooks = '$baseUrl/books-json/';
